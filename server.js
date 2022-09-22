4a0b7e3de65306b23eda3b52ca483ab81ea6c814
gourdine <ch1ck3n09@gmail.com>
2022-09-22 09:40:01 -0500

    Added readme and starter config

--- server.js
+++ server.js
@@ -1,14 +1,97 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
+const errorHandler = require('./middleware/error');
+const fileupload = require('express-fileupload');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 
-// Load env vars
+//Load env vars
 dotenv.config({ path: './config/config.env'});
+
+//connect to database
+connectDB();
+
+//route files
+const bootcamps = require('./routes/bootcamps');
+const courses = require('./routes/courses');
+const auth = require('./routes/auth');
+const users = require('./routes/users');
+const reviews = require('./routes/reviews');
 
 const app = express();
 
+//Body parser
+app.use(express.json());
+
+// Cookie parser
+app.use(cookieParser());
+
+//dev logging middleware
+if(process.env.NODE_ENV === 'development'){
+  app.use(morgan('dev'));
+}
+
+//File uploading
+app.use(fileupload());
+
+// Sanitize data
+app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 mins
+  max: 100
+});
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
+
+//Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+app.use('/api/v1/courses', courses);
+app.use('/api/v1/auth', auth);
+app.use('/api/v1/users', users);
+app.use('/api/v1/reviews', reviews);
+
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running on ${process.env.NODE_ENV} mode on port ${PORT}`)
-  )
+  console.log(`Server running on ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+  );
+
+  //handle unhandled promise rejects
+  process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    //close server exit process
+    server.close(() => process.exit(1));
+  });
+
+
+
+
+  // https://github.com/bradtraversy/devcamper-api
